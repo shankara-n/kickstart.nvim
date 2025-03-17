1f105c4c0b7be32dc2dab1de3e6ca27e71259c28
Shankara Narayana <slimruu@amazon.com>
2025-03-17 00:54:01 +0000

    Search continue and search replace remap

--- init.lua
+++ init.lua
@@ -325,6 +325,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'mfussenegger/nvim-jdtls',
+  'mg979/vim-visual-multi',
   'APZelos/blamer.nvim',
   'pocco81/autosave.nvim',
   -- NOTE: Plugins can also be added by using a table,
@@ -427,8 +428,13 @@ require('lazy').setup({
   },
   {
     "nvim-pack/nvim-spectre",
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        require("spectre").setup()
+      require("spectre").setup()
+
+      vim.keymap.set('n', '<leader>sr', function()
+        require('spectre').open()
+      end, { desc = '[S]earch [R]eplace (Spectre)' })
     end,
   },
 
@@ -452,7 +458,7 @@ require('lazy').setup({
         -- This is only run then, not every time Neovim starts up.
         build = 'make',
 
-        -- `cond` is a condition used to determine whether this plugin should be
+       -- `cond` is a condition used to determine whether this plugin should be
         -- installed and loaded.
         cond = function()
           return vim.fn.executable 'make' == 1
@@ -532,7 +538,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sc', builtin.resume, { desc = '[S]earch [C]ontinue' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
